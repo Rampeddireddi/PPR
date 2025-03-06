9617727b2f141221f5e4531a03f1adaee452c808
RAM PEDDIREDDI <144416585+Rampeddireddi@users.noreply.github.com>
2025-03-06 18:53:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     
      <div className='w-full h-full flex flex-col items-center relative overflow-x-hidden'>
-    <div className="fixed top-0 left-0 z-[-2] h-screen w-screen bg-black bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(255,140,0,0.3),rgba(0,0,0,1))]"></div><Navbar></Navbar>
+    <div className="fixed top-0 left-0 z-[-2] h-screen w-screen bg-black bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(255,140,0,0.3),rgba(0,0,0,1))]"></div>
    <Navbar></Navbar>
    <Hero/>
    <About/>
